8e798a1e8981d852bed6f6d77c6d9412b5f836f9
Erfan <Erfanabedi.78@gmail.com>
2019-08-07 15:15:00 +0430

    Added http server/client, it's working but still having problems with goland. :))

--- util/equation.go
+++ util/equation.go
@@ -1,8 +1,66 @@
 package util
 
-// is a representation of an equation.
-type Equation struct{
-	A float64
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+//Equation is a representation of an equation.
+type Equation struct {
+	A   float64
 	Fix string
-	B float64
+	B   float64
+}
+
+//SetField 1212121212  dsdsd
+func SetField(strct interface{}, name string, value interface{}) error {
+	structValue := reflect.ValueOf(strct).Elem()
+	structFieldValue := structValue.FieldByName(name)
+
+	// Put breakpoint here!
+
+	if !structFieldValue.IsValid() {
+		return fmt.Errorf("no such field: %s in obj", name)
+	}
+
+	if !structFieldValue.CanSet() {
+		return fmt.Errorf("cannot set %s field value", name)
+	}
+
+	structFieldType := structFieldValue.Type()
+	val := reflect.ValueOf(value)
+	if structFieldType != val.Type() {
+		invalidTypeError := errors.New("Provided value type didn't match obj field type")
+		return invalidTypeError
+	}
+
+	structFieldValue.Set(val)
+	return nil
+}
+
+//FillStruct 123
+func FillStruct(s *Equation, m map[string]interface{}) error {
+	for k, v := range m {
+		err := SetField(s, k, v)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//Calculate calculates
+func Calculate(eq Equation) (ans float64) {
+	switch eq.Fix {
+	case "+":
+		ans = eq.A + eq.B
+	case "-":
+		ans = eq.A - eq.B
+	case "*":
+		ans = eq.A * eq.B
+	case "/":
+		ans = eq.A / eq.B
+	}
+	return
 }
